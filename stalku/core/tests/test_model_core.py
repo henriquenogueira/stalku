fa6579e46361211e0e132a03eeea65d9bab3a54b
Henrique Nogueira <hrqnogueira@gmail.com>
2016-03-05 17:31:03 -0300

    Added comments to test and created test on the full_academic_record attribute.

--- stalku/core/tests/test_model_core.py
+++ stalku/core/tests/test_model_core.py
@@ -8,18 +8,25 @@ class StudentModelTest(TestCase):
     def setUp(self):
         self.obj = Student(
             name='Henrique Gaspar Nogueira',
-            academic_record='073203'
+            academic_record='73203'
         )
         self.obj.save()
 
     def test_create(self):
+        """Make sure the student can be saved on the DB"""
         self.assertTrue(Student.objects.exists())
 
     def test_created_at(self):
+        """Test created_at attribute type"""
         self.assertIsInstance(self.obj.created_at, datetime)
 
     def test_name_str(self):
+        """Checking student name"""
         self.assertEqual('Henrique Gaspar Nogueira', self.obj.name)
+
+    def test_full_academic_record(self):
+        """Test length of the academic record"""
+        self.assertEqual('073203', self.obj.full_academic_record)
 
 
 class LectureModelTest(TestCase):
@@ -40,7 +47,9 @@ class LectureModelTest(TestCase):
         self.obj.save()
 
     def test_create(self):
+        """Make sure the lecture can be saved on the DB"""
         self.assertTrue(Lecture.objects.exists())
 
     def test_created_at(self):
+        """Test created_at attribute type"""
         self.assertIsInstance(self.obj.created_at, datetime)
